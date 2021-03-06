bafc0384213b64957e1bd06282c4183222770cc4
Neil Tallim <flan@uguu.ca>
2021-03-06 00:23:53 -0700

    More notes

--- golang/tyuo/logic/common.go
+++ golang/tyuo/logic/common.go
@@ -17,12 +17,22 @@ const sentenceBoundary int = -2147483648
 //each successive node, each sibling will be tried in order received from the
 //database (which is random), and any terminals encountered will produce a
 //new forward/reverse option
+
+//NOTE: when looking at transition options, select searchBranches in total, but
+//choose anything containing an unencountered keyword first, before doing
+//a random/weighted pick of the remainder
+//to make this efficient, the keyword-set should be a hash-set, maybe
+//with a boolean value set to true/false in each node, if it's expensive to copy it;
+//flipped values could be tracked in a slice and restored before returning
+
 //when reaching a stopping point, as the stack is retraversed upwards,
 //the next sibling is selected only if there are no candidates from
 //traversing the previous branch.
 
 //after falling back to a trigram search, the first terminal found, beyond minDepth,
 //will end that discovery path
+
+//when doing a markov walk, choose anything in the keyword set first, if possible
 
 //TODO: these should be flag-parameters
 const searchBranches int32 = 5
